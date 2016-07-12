82f844b75bcac50ca48937b5a967bb057c266384
Denis Aoki <dnsaoki@gmail.com>
2016-07-12 00:35:11 -0300

    update paloma.json and db/storage.go

--- db/storage.go
+++ db/storage.go
@@ -1,13 +1,9 @@
-package main
+package db
 
 import (
-	// "encoding/json"
 	"fmt"
 	"gopkg.in/mgo.v2"
-	// "gopkg.in/mgo.v2/bson"
-	// "io/ioutil"
-	// "os"
-	// "time"
+	"gopkg.in/mgo.v2/bson"
 
 	"github.com/tcc-denis-raul/Projeto-api/conf"
 )
@@ -20,12 +16,6 @@ const (
 type DB struct {
 	session *mgo.Session
 	DBName  string
-}
-
-func main() {
-	a, b := GetSession()
-	fmt.Println(a, b)
-
 }
 
 func GetSession() (DB, error) {
@@ -48,4 +38,35 @@ func GetSession() (DB, error) {
 	session.SetMode(mgo.Monotonic, true)
 
 	return DB{session, name}, nil
+}
+
+type Courses struct {
+	ID          bson.ObjectId `bson:"_id,omitempty"`
+	Name        string
+	Based       []string
+	PriceReal   []float64
+	PriceDolar  []float64
+	Dynamic     []string
+	Platform    []string
+	Url         string
+	Extra       []string
+	Description string
+}
+
+/*
+INPUT: type and course
+OUTPUT: list of all courses of type and course
+*/
+func GetCourses(typ, course string) ([]Courses, error) {
+	db, err := GetSession()
+	if err != nil {
+		return nil, err
+	}
+	defer db.session.Close()
+	var data []Courses
+	err = db.session.DB(db.DBName).C(fmt.Sprintf("%s_%s", typ, course)).Find(nil).All(&data)
+	if err != nil {
+		return nil, err
+	}
+	return data, nil
 }
